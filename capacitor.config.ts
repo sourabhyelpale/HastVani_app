cee0f87fcb5279f8484504710c89caa7d4c5b6b3
Abhishek Ujale <abhishekujale.kitcse18@gmail.com>
2026-05-05 21:19:41 +0530

    fix: build of nextJS version

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'myapp',
+  appId: 'com.hastvaani.app',
+  appName: 'HastVaani',
   webDir: 'out'
 };
 
